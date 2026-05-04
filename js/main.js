1a2c05b5b011dffb93c8262a0b66fa093eb228c2
ny-margarita-g <margarita.grigoreva@noseryoung.ch>
2026-05-04 11:14:43 +0200

    added main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function openModal() {
+  document.getElementById('modal').classList.add('open');
+}
+
+function closeModal() {
+  document.getElementById('modal').classList.remove('open');
+}
+
+function toggleMenu() {
+  document.getElementById('main-nav').classList.toggle('open');
+}
+ 
